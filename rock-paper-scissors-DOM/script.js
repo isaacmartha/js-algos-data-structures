6ad4fc14bf3e0eb6bbd743f9c6fe51be39e86132
isaacmartha <isaaceditar@gmail.com>
2024-08-07 20:58:55 -0300

    Making the basis of Script

--- rock-paper-scissors-DOM/script.js
+++ rock-paper-scissors-DOM/script.js
@@ -1,5 +1,43 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-  
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
   }
-  console.log(getRandomComputerResult());
+  
+  function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+  }
+  
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+  
+    if (hasPlayerWonTheRound(userOption, computerResult)) {
+      playerScore++;
+      return `Player wins! ${userOption} beats ${computerResult}`;
+    } else if (computerResult === userOption) {
+      return `It's a tie! Both chose ${userOption}`;
+    } else {
+      computerScore++;
+      return `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+  }
+  
+  const playerScoreSpanElement = document.getElementById("player-score");
+  const computerScoreSpanElement = document.getElementById("computer-score");
+  const roundResultsMsg = document.getElementById("results-msg");
+  
+  function showResults(userOption) {
+    const resultMessage = getRoundResults(userOption);
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    roundResultsMsg.innerText = resultMessage;
+  };
+  
+  showResults("Rock");
